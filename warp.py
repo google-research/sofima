241587556c3e17accdc20f784edb9d0b87fbe816
Michal Januszewski <mjanusz@google.com>
2023-03-03 13:08:09 -0800

    Add support for explicit tile masks.
    PiperOrigin-RevId: 513903593

--- warp.py
+++ warp.py
@@ -14,8 +14,10 @@
 # limitations under the License.
 """Utilities for warping image and point data between coordinate systems."""
 
+from __future__ import annotations
+
 from concurrent import futures
-from typing import Any, Dict, Optional, Sequence, Tuple, Union
+from typing import Any, Optional, Sequence, Union
 from connectomics.common import bounding_box
 from connectomics.common import box_generator
 from connectomics.segmentation import labels
@@ -37,7 +39,7 @@ def _cvx2_interpolation(inter_scheme: str):
       'nearest': cvx2.INTER_NEAREST,
       'linear': cvx2.INTER_LINEAR,
       'cubic': cvx2.INTER_CUBIC,
-      'lanczos': cvx2.INTER_LANCZOS4
+      'lanczos': cvx2.INTER_LANCZOS4,
   }
   return inter_map[inter_scheme]
 
@@ -51,19 +53,23 @@ def _relabel_segmentation(data, orig_to_low, old_uids):
 
   orig_ids, low_ids = zip(*orig_to_low)
   return labels.relabel(
-      data.astype(np.uint64), np.array(low_ids, dtype=np.uint64),
-      np.array(orig_ids, dtype=np.uint64))
+      data.astype(np.uint64),
+      np.array(low_ids, dtype=np.uint64),
+      np.array(orig_ids, dtype=np.uint64),
+  )
 
 
-def warp_subvolume(image: np.ndarray,
-                   image_box: bounding_box.BoundingBoxBase,
-                   coord_map: np.ndarray,
-                   map_box: bounding_box.BoundingBoxBase,
-                   stride: float,
-                   out_box: bounding_box.BoundingBoxBase,
-                   interpolation: Optional[str] = None,
-                   offset: float = 0.,
-                   parallelism: int = 1) -> np.ndarray:
+def warp_subvolume(
+    image: np.ndarray,
+    image_box: bounding_box.BoundingBoxBase,
+    coord_map: np.ndarray,
+    map_box: bounding_box.BoundingBoxBase,
+    stride: float,
+    out_box: bounding_box.BoundingBoxBase,
+    interpolation: Optional[str] = None,
+    offset: float = 0.0,
+    parallelism: int = 1,
+) -> np.ndarray:
   """Warps a subvolume of data according to a coordinate map.
 
   Args:
@@ -110,28 +116,32 @@ def warp_subvolume(image: np.ndarray,
       if image.max() >= 2**16:
         raise ValueError(
             'Image warping supported up to uint16 only. For segmentation data, '
-            'use uint64.')
+            'use uint64.'
+        )
       image = image.astype(np.uint16)
 
   skipped_sections = frozenset(
-      np.where(np.all(np.isnan(coord_map), axis=(0, 2, 3)))[0])
+      np.where(np.all(np.isnan(coord_map), axis=(0, 2, 3)))[0]
+  )
 
   # Convert values within the coordinate map so that they are
   # within the local coordinate system of 'image'.
   abs_map = map_utils.to_absolute(coord_map, stride)
-  abs_map += (map_box.start[:2] * stride - image_box.start[:2] +
-              offset).reshape(2, 1, 1, 1)
+  abs_map += (
+      map_box.start[:2] * stride - image_box.start[:2] + offset
+  ).reshape(2, 1, 1, 1)
 
   # Coordinates of the map nodes within the local coordinate
   # system of 'out_box'.
-  map_y, map_x = np.ogrid[:coord_map.shape[2], :coord_map.shape[3]]
+  map_y, map_x = np.ogrid[: coord_map.shape[2], : coord_map.shape[3]]
   map_y = (map_y + map_box.start[1]) * stride - out_box.start[1] + offset
   map_x = (map_x + map_box.start[0]) * stride - out_box.start[0] + offset
   map_points = (map_y.ravel(), map_x.ravel())
 
   warped = np.zeros(
-      shape=[image.shape[0]] + list(out_box.size[::-1]), dtype=image.dtype)
-  out_y, out_x = np.mgrid[:out_box.size[1], :out_box.size[0]]
+      shape=[image.shape[0]] + list(out_box.size[::-1]), dtype=image.dtype
+  )
+  out_y, out_x = np.mgrid[: out_box.size[1], : out_box.size[0]]
 
   try:
     maptype = cvx2.CVX_16SC2
@@ -140,9 +150,11 @@ def warp_subvolume(image: np.ndarray,
 
   def _warp_section(z):
     dense_x = interpolate.RegularGridInterpolator(
-        map_points, abs_map[0, z, ...], bounds_error=False, fill_value=None)
+        map_points, abs_map[0, z, ...], bounds_error=False, fill_value=None
+    )
     dense_y = interpolate.RegularGridInterpolator(
-        map_points, abs_map[1, z, ...], bounds_error=False, fill_value=None)
+        map_points, abs_map[1, z, ...], bounds_error=False, fill_value=None
+    )
 
     # dxy: [0 .. out_box.size] -> [coord within image]
     dx = dense_x((out_y, out_x)).astype(np.float32)
@@ -152,11 +164,13 @@ def warp_subvolume(image: np.ndarray,
         dx,
         dy,
         dstmap1type=maptype,
-        nninterpolation=(interpolation == cvx2.INTER_NEAREST))
+        nninterpolation=(interpolation == cvx2.INTER_NEAREST),
+    )
 
     for c in range(0, image.shape[0]):
       warped[c, z, ...] = cvx2.remap(
-          image[c, z, ...], dx, dy, interpolation=interpolation)
+          image[c, z, ...], dx, dy, interpolation=interpolation
+      )
 
   fs = set()
   with futures.ThreadPoolExecutor(max_workers=parallelism) as exc:
@@ -179,17 +193,19 @@ def warp_subvolume(image: np.ndarray,
   return warped
 
 
-def ndimage_warp(image: np.ndarray,
-                 coord_map: np.ndarray,
-                 stride: Sequence[float],
-                 work_size: Sequence[int],
-                 overlap: Sequence[int],
-                 order=1,
-                 map_coordinates=ndimage.map_coordinates,
-                 image_box: Optional[bounding_box.BoundingBoxBase] = None,
-                 map_box: Optional[bounding_box.BoundingBoxBase] = None,
-                 out_box: Optional[bounding_box.BoundingBoxBase] = None,
-                 parallelism: int = 1) -> np.ndarray:
+def ndimage_warp(
+    image: np.ndarray,
+    coord_map: np.ndarray,
+    stride: Sequence[float],
+    work_size: Sequence[int],
+    overlap: Sequence[int],
+    order=1,
+    map_coordinates=ndimage.map_coordinates,
+    image_box: Optional[bounding_box.BoundingBoxBase] = None,
+    map_box: Optional[bounding_box.BoundingBoxBase] = None,
+    out_box: Optional[bounding_box.BoundingBoxBase] = None,
+    parallelism: int = 1,
+) -> np.ndarray:
   """Warps a subvolume of data using ndimage.map_coordinates.
 
   Args:
@@ -232,8 +248,9 @@ def ndimage_warp(image: np.ndarray,
     if image_box is None:
       raise ValueError('image_box has to be specified when map_box is used.')
 
-    src_map += (map_box.start[:dim] * stride[::-1] -
-                image_box.start[:dim]).reshape(dim, 1, 1, 1)
+    src_map += (
+        map_box.start[:dim] * stride[::-1] - image_box.start[:dim]
+    ).reshape(dim, 1, 1, 1)
 
   sub_dim = 0
   image_size_xyz = image.shape[::-1]
@@ -253,7 +270,8 @@ def ndimage_warp(image: np.ndarray,
       outer_box=bounding_box.BoundingBox(start=(0, 0, 0), size=out_box.size),
       box_size=work_size,
       box_overlap=overlap,
-      back_shift_small_boxes=True)
+      back_shift_small_boxes=True,
+  )
 
   if map_box is not None:
     assert out_box is not None
@@ -264,8 +282,10 @@ def ndimage_warp(image: np.ndarray,
   def _warp_box(i):
     in_sub_box = calc.generate(i)[1]
     sel = [
-        np.s_[start:end] for start, end in zip(in_sub_box.start[::-1][sub_dim:],
-                                               in_sub_box.end[::-1][sub_dim:])
+        np.s_[start:end]
+        for start, end in zip(
+            in_sub_box.start[::-1][sub_dim:], in_sub_box.end[::-1][sub_dim:]
+        )
     ]
     src_coords = np.mgrid[sel]
     src_coords = [(c - o) / s for c, s, o in zip(src_coords, stride, offset)]
@@ -281,7 +301,8 @@ def ndimage_warp(image: np.ndarray,
     rel_box = out_sub_box.translate(-in_sub_box.start)
 
     warped[out_sub_box.to_slice3d()[sub_dim:]] = sub_warped[
-        rel_box.to_slice3d()[sub_dim:]]
+        rel_box.to_slice3d()[sub_dim:]
+    ]
 
   fs = set()
   with futures.ThreadPoolExecutor(max_workers=parallelism) as exc:
@@ -298,20 +319,24 @@ def ndimage_warp(image: np.ndarray,
 
 
 def render_tiles(
-    tiles: Dict[Tuple[int, int], np.ndarray],
-    coord_maps: Dict[Tuple[int, int], np.ndarray],
-    stride: Tuple[int, int] = (20, 20),
+    tiles: dict[tuple[int, int], np.ndarray],
+    coord_maps: dict[tuple[int, int], np.ndarray],
+    stride: tuple[int, int] = (20, 20),
     margin: int = 50,
     parallelism: int = 1,
     width: Optional[int] = None,
     height: Optional[int] = None,
     use_clahe: bool = False,
     clahe_kwargs: ... = None,
-    margin_overrides: Optional[Dict[Tuple[int, int], Tuple[int, int, int,
-                                                           int]]] = None,
-    return_warped_tiles: bool = False
-) -> Union[tuple[np.ndarray, np.ndarray], tuple[np.ndarray, np.ndarray, dict[
-    tuple[int, int], Any]]]:
+    margin_overrides: Optional[
+        dict[tuple[int, int], tuple[int, int, int, int]]
+    ] = None,
+    return_warped_tiles: bool = False,
+    tile_masks: Optional[dict[tuple[int, int], np.ndarray]] = None
+) -> Union[
+    tuple[np.ndarray, np.ndarray],
+    tuple[np.ndarray, np.ndarray, dict[tuple[int, int], Any]],
+]:
   """Warps a collection of tiles into a larger image.
 
   All values in the 'tiles' and 'positions' maps are assumed to
@@ -336,6 +361,9 @@ def render_tiles(
       default provided in 'margin'.
     return_warped_tiles: whether to return the warped tiles and their relative
       positions.
+    tile_masks: map from (x, y) tile coordinates to map arrays (same shape as
+      tile images); if present, the elements of the mask evaluating to True
+      define the pixels that should be rendered
 
   Returns:
     tuple of:
@@ -351,15 +379,18 @@ def render_tiles(
   """
   if stride[0] != stride[1]:
     raise NotImplementedError(
-        'Currently only equal strides in XY are supported.')
+        'Currently only equal strides in XY are supported.'
+    )
 
   any_tile = next(iter(tiles.values()))
   img_yx = any_tile.shape
   image_box = bounding_box.BoundingBox(
-      start=(0, 0, 0), size=(img_yx[1], img_yx[0], 1))
+      start=(0, 0, 0), size=(img_yx[1], img_yx[0], 1)
+  )
   map_yx = next(iter(coord_maps.values())).shape[-2:]
   map_box = bounding_box.BoundingBox(
-      start=(0, 0, 0), size=(map_yx[1], map_yx[0], 1))
+      start=(0, 0, 0), size=(map_yx[1], map_yx[0], 1)
+  )
 
   warped_image_map = {}
 
@@ -383,6 +414,12 @@ def render_tiles(
     if img is None:
       return
 
+    tile_mask = None
+    if tile_masks is not None:
+      tile_mask = tile_masks.get((tile_x, tile_y), None)
+    if tile_mask is not None:
+      tile_mask = (tile_mask == 0)
+
     tg_box = map_utils.outer_box(coord_map, map_box, stride[0])
     # Add context to avoid rounding issues.
     tg_box = tg_box.adjusted_by(start=(-1, -1, 0), end=(1, 1, 0))
@@ -396,26 +433,37 @@ def render_tiles(
 
     if margin_overrides is not None and (tile_x, tile_y) in margin_overrides:
       mo = margin_overrides[tile_x, tile_y]
-      mask[mo[0]:-(mo[1] + 1), mo[2]:-(mo[3] + 1)] = 1
+      mask[mo[0] : -(mo[1] + 1), mo[2] : -(mo[3] + 1)] = 1
     else:
-      mask[margin:-(margin + 1), margin:-(margin + 1)] = 1
+      mask[margin : -(margin + 1), margin : -(margin + 1)] = 1
 
     if use_clahe:
-      img = (skimage.exposure.equalize_adapthist(img, **clahe_kwargs) *
-             np.iinfo(img.dtype).max).astype(img.dtype)
+      img = (
+          skimage.exposure.equalize_adapthist(img, **clahe_kwargs)
+          * np.iinfo(img.dtype).max
+      ).astype(img.dtype)
+
+    if tile_mask is not None:
+      mask[tile_mask] = 0
 
     to_warp = np.concatenate(
         [img[np.newaxis, np.newaxis, ...], mask[np.newaxis, np.newaxis, ...]],
-        axis=0)
+        axis=0,
+    )
 
-    out_box = image_box.translate(((tg_box.start[0] + 1) * stride[1],
-                                   (tg_box.start[1] + 1) * stride[0], 0))
+    out_box = image_box.translate((
+        (tg_box.start[0] + 1) * stride[1],
+        (tg_box.start[1] + 1) * stride[0],
+        0,
+    ))
     out_box = bounding_box.BoundingBox(
         start=out_box.start,
-        size=(tg_box.size[0] * stride[1], tg_box.size[1] * stride[0], 1))
+        size=(tg_box.size[0] * stride[1], tg_box.size[1] * stride[0], 1),
+    )
 
     warped_img, warped_mask = warp_subvolume(
-        to_warp, image_box, inverted_map, tg_box, stride[0], out_box=out_box)
+        to_warp, image_box, inverted_map, tg_box, stride[0], out_box=out_box
+    )
 
     warped_img = warped_img[0, ...]
     warped_mask = warped_mask[0, ...].astype(bool)
@@ -436,16 +484,17 @@ def render_tiles(
       warped_mask = warped_mask[-y0:, :]
       y0 = 0
 
-    out = ret[y0:y0 + warped_img.shape[0], x0:x0 + warped_img.shape[1]]
+    out = ret[y0 : y0 + warped_img.shape[0], x0 : x0 + warped_img.shape[1]]
     os = out.shape
-    warped_mask = warped_mask[:os[0], :os[1]]
-    warped_img = warped_img[:os[0], :os[1]]
+    warped_mask = warped_mask[: os[0], : os[1]]
+    warped_img = warped_img[: os[0], : os[1]]
 
     if return_warped_tiles:
       warped_image_map[(tile_x, tile_y)] = x0, y0, warped_img
 
-    ret_mask[y0:y0 + warped_img.shape[0],
-             x0:x0 + warped_img.shape[1]][warped_mask] = True
+    ret_mask[y0 : y0 + warped_img.shape[0], x0 : x0 + warped_img.shape[1]][
+        warped_mask
+    ] = True
 
     # If we failed to render any locations in warped_img, do not copy them to
     # the canvas.
@@ -457,7 +506,8 @@ def render_tiles(
     with futures.ThreadPoolExecutor(max_workers=parallelism) as exc:
       for (x, y), coord_map in coord_maps.items():
         fs.add(
-            exc.submit(_render_tile, tile_x=x, tile_y=y, coord_map=coord_map))
+            exc.submit(_render_tile, tile_x=x, tile_y=y, coord_map=coord_map)
+        )
 
       for f in futures.as_completed(fs):
         f.result()
