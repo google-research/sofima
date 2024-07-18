968af3b57e385918c68a1f1ca880b0ebbea1092b
Tim Blakely <blakely@google.com>
2024-07-18 16:30:54 -0700

    - Convert EstimateFlow to use Dataclasses as a configuration parameter, and update pipelines to support. - Bugfix: Adjust EstimateFlow's overlap + expected bounding box to account for the internal stride, resulting in output that no longer overlaps. This overlap caused a significant performance hit while writing distributed volumes with TensorStore.
    PiperOrigin-RevId: 653792180

--- processor/flow.py
+++ processor/flow.py
@@ -14,9 +14,11 @@
 # limitations under the License.
 """Flow field estimation from SOFIMA."""
 
+import dataclasses
 from typing import Optional
 
 from connectomics.common import bounding_box
+from connectomics.common import utils
 from connectomics.volume import mask
 from connectomics.volume import subvolume
 from connectomics.volume import subvolume_processor
@@ -46,104 +48,108 @@ class EstimateFlow(subvolume_processor.SubvolumeProcessor):
   centered at 'x' is stored at 'x' // patch_size.
   """
 
-  _patch_size: int
-  _stride: int
-  _z_stride: int
-  _fixed_current: bool
-  _batch_size: int
-  _mask_config: Optional[mask.MaskConfigs]
-  _sel_mask_config: Optional[mask.MaskConfigs]
-  _mask_only_for_patch_selection: bool
+  @dataclasses.dataclass(eq=True)
+  class EstimateFlowConfig(utils.NPDataClassJsonMixin):
+    """Configuration for EstimateFlow."""
 
-  def __init__(self,
-               patch_size: int,
-               stride: int,
-               z_stride: int = 1,
-               fixed_current: bool = False,
-               mask_configs: Optional[mask.MaskConfigs] = None,
-               mask_only_for_patch_selection: bool = False,
-               selection_mask_configs=None,
-               batch_size=1024,
-               input_volinfo=None):
+    # Patch size in pixels, divisible by 'stride'
+    patch_size: int
+    # XY stride size in pixels
+    stride: int
+    # Z stride size in pixels (Δz)
+    z_stride: int = 1
+    # Whether to compute flow against a fixed current section (first/last
+    # section of the subvolume for negative/positive z_stride respectively);
+    # this is useful for coming-in regions.
+    fixed_current: bool = False
+    # mask.MaskConfigs specifying a mask to exclude some voxels from the flow
+    # calculation; this mask should have the same resolution and geometry as the
+    # input data volume.
+    mask_configs: Optional[mask.MaskConfigs] = None
+    # Whether to only use mask to decide for which patch pairs to compute flow.
+    mask_only_for_patch_selection: bool = False
+    # MaskConfigs in text format specifying a mask the positive entries of which
+    # indicate locations for which flow should be computed; this mask should
+    # have the same resolution and geometry as the output flow volume.
+    selection_mask_configs: Optional[mask.MaskConfigs] = None
+    # Max number of patches to process in parallel input_volinfo: VolumeInfo for
+    # the input volume.
+    batch_size: int = 1024
+
+  _config: EstimateFlowConfig
+
+  def __init__(self, config: EstimateFlowConfig, input_volinfo_or_ts_spec=None):
     """Constructor.
 
     Args:
-      patch_size: patch size in pixels, divisible by 'stride'
-      stride: XY stride size in pixels
-      z_stride: Z stride size in pixels (Δz)
-      fixed_current: whether to compute flow against a fixed current section
-        (first/last section of the subvolume for negative/positive z_stride
-        respectively); this is useful for coming-in regions
-      mask_configs: MaskConfigs proto in text format specifying a mask to
-        exclude some voxels from the flow calculation
-      mask_only_for_patch_selection: whether to only use mask to decide for
-        which patch pairs to compute flow
-      selection_mask_configs: MaskConfigs in text format specifying a mask the
-        positive entries of which indicate locations for which flow should be
-        computed; this mask should have the same resolution and geometry as the
-        output flow volume
-      batch_size: max number of patches to process in parallel
-      input_volinfo: VolumeInfo for the input volume
+      config: Parameters for EstimateFlow
+      input_volinfo_or_ts_spec: unused
     """
-    self._patch_size = patch_size
-    self._stride = stride
-    assert self._patch_size % self._stride == 0
 
-    self._z_stride = z_stride
-    self._fixed_current = fixed_current
-    self._batch_size = batch_size
+    self._config = config
 
-    self._mask_config = mask_configs
-
-    self._sel_mask_config = selection_mask_configs
-
-    self._mask_only_for_patch_selection = mask_only_for_patch_selection
-    self.input_volinfo = input_volinfo
+    assert config.patch_size % config.stride == 0
 
   def output_type(self, input_type):
     return np.float32
 
   def subvolume_size(self):
-    size = self._patch_size * 8
+    size = self._config.patch_size * 8
     return subvolume_processor.SuggestedXyz(size, size, 16)
 
   def context(self):
-    pre = self._patch_size // 2
-    post = self._patch_size - pre
-    if self._fixed_current:
-      if self._z_stride > 0:
-        return (pre, pre, 0), (post, post, self._z_stride)
+    config = self._config
+    pre = config.patch_size // 2
+    post = config.patch_size - pre
+    if config.fixed_current:
+      if config.z_stride > 0:
+        return (pre, pre, 0), (post, post, config.z_stride)
       else:
-        return (pre, pre, -self._z_stride), (post, post, 0)
+        return (pre, pre, -config.z_stride), (post, post, 0)
     else:
-      if self._z_stride > 0:
-        return (pre, pre, self._z_stride), (post, post, 0)
+      if config.z_stride > 0:
+        return (pre, pre, config.z_stride), (post, post, 0)
       else:
-        return (pre, pre, 0), (post, post, -self._z_stride)
+        return (pre, pre, 0), (post, post, -config.z_stride)
 
   def num_channels(self, input_channels):
     del input_channels
-    return flow_field.JAXMaskedXCorrWithStatsCalculator.non_spatial_flow_channels + 2
+    return (
+        flow_field.JAXMaskedXCorrWithStatsCalculator.non_spatial_flow_channels
+        + 2
+    )
 
   def pixelsize(self, psize):
     psize = np.asarray(psize).copy().astype(np.float32)
-    psize[:2] *= self._stride
+    psize[:2] *= self._config.stride
     return psize
 
   def process(self, subvol: subvolume.Subvolume) -> subvolume.Subvolume:
     # TODO(blakely): Determine if Dask supports metrics, and if so, create a
     # shim that supports both Beam and Dask metrics.
+    config = self._config
 
     assert subvol.data.shape[0], 'Input volume should have 1 channel.'
     image = subvol.data[0, ...]
     sel_mask = initial_mask = None
 
-    if self._mask_config is not None:
-      initial_mask = mask.build_mask(self._mask_config, subvol.bbox)
+    if config.mask_configs is not None:
+      initial_mask = mask.build_mask(config.mask_configs, subvol.bbox)
 
-      if self._sel_mask_config is not None:
-        sel_box = subvol.bbox.scale([1.0 / self._stride, 1.0 / self._stride, 1])
-        sel_mask = mask.build_mask(self._sel_mask_config, sel_box)
+    if config.selection_mask_configs is not None:
+      cropped_bbox = self.crop_box(subvol.bbox)
+      sel_start = [
+          cropped_bbox.start[0] / config.stride,
+          cropped_bbox.start[1] / config.stride,
+          subvol.bbox.start[2],
+      ]
+      xy = np.array([1, 1, 0])
+      scale = np.array([config.stride, config.stride, 1])
+      sel_size = (
+          subvol.bbox.size - xy * config.patch_size + xy * config.stride
+      ) / scale
+      sel_box = bounding_box.BoundingBox(sel_start, sel_size)
+      sel_mask = mask.build_mask(config.selection_mask_configs, sel_box)
 
     def _estimate_flow(z_prev, z_curr):
       mask_prev = mask_curr = None
@@ -161,19 +167,20 @@ class EstimateFlow(subvolume_processor.SubvolumeProcessor):
       return mfc.flow_field(
           prev,
           curr,
-          self._patch_size,
-          self._stride,
+          config.patch_size,
+          config.stride,
           mask_prev,
           mask_curr,
-          mask_only_for_patch_selection=self._mask_only_for_patch_selection,
+          mask_only_for_patch_selection=config.mask_only_for_patch_selection,
           selection_mask=smask,
-          batch_size=self._batch_size)
+          batch_size=config.batch_size,
+      )
 
     mfc = flow_field.JAXMaskedXCorrWithStatsCalculator()
     flows = []
 
-    if self._fixed_current:
-      if self._z_stride > 0:
+    if config.fixed_current:
+      if config.z_stride > 0:
         rng = range(0, image.shape[0] - 1)
         z_curr = image.shape[0] - 1
       else:
@@ -182,13 +189,13 @@ class EstimateFlow(subvolume_processor.SubvolumeProcessor):
       for z_prev in rng:
         flows.append(_estimate_flow(z_prev, z_curr))
     else:
-      if self._z_stride > 0:
-        rng = range(0, image.shape[0] - self._z_stride)
+      if config.z_stride > 0:
+        rng = range(0, image.shape[0] - config.z_stride)
       else:
-        rng = range(-self._z_stride, image.shape[0])
+        rng = range(-config.z_stride, image.shape[0])
 
       for z in rng:
-        flows.append(_estimate_flow(z, z + self._z_stride))
+        flows.append(_estimate_flow(z, z + config.z_stride))
 
     ret = np.array(flows)
 
@@ -197,19 +204,45 @@ class EstimateFlow(subvolume_processor.SubvolumeProcessor):
     #   Δz < 0: box.start.z
     out_box = self.crop_box(subvol.bbox)
     out_box = bounding_box.BoundingBox(
-        start=out_box.start // [self._stride, self._stride, 1],
-        size=[ret.shape[-1], ret.shape[-2], out_box.size[2]])
+        start=out_box.start // [config.stride, config.stride, 1],
+        size=[ret.shape[-1], ret.shape[-2], out_box.size[2]],
+    )
 
     expected_box = self.expected_output_box(subvol.bbox)
     if out_box != expected_box:
       raise ValueError(
           f'Bounding box does not match expected output_box {out_box} vs '
-          f'{expected_box}')
+          f'{expected_box}'
+      )
 
     return subvolume.Subvolume(np.transpose(ret, (1, 0, 2, 3)), out_box)
 
+  # Because mfc.flow_field does not take into account the standard subvolume
+  # processor overlap schemes - the latter knows nothing about the internal
+  # stride count, so cannot take it into account when calculating overlap - we
+  # need to adjust the calculations manually to avoid duplicating output data
+  # which can potentially cause write contention. We must modify `overlap` and
+  # `expected_output_box` to take this into account.
+  def overlap(self) -> subvolume_processor.TupleOrSuggestedXyz:
+    overlap = super(EstimateFlow, self).overlap()
+    return (
+        overlap[0] - self._config.stride,
+        overlap[1] - self._config.stride,
+        overlap[2],
+    )
+
   def expected_output_box(
-      self, box: bounding_box.BoundingBoxBase) -> bounding_box.BoundingBoxBase:
+      self, box: bounding_box.BoundingBoxBase
+  ) -> bounding_box.BoundingBoxBase:
+
     scale_factor = 1 / self.pixelsize(np.repeat(1, len(box.size)))
     cropped_box = self.crop_box(box)
-    return cropped_box.scale(list(scale_factor))
+    scaled_box = cropped_box.scale(list(scale_factor))
+    start = scaled_box.start
+    size = scaled_box.size
+    size[:2] = (
+        np.array(self.subvolume_size()[:2])
+        - self._config.patch_size
+        + self._config.stride
+    ) // self._config.stride
+    return bounding_box.BoundingBox(start, size)
